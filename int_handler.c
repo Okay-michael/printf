dbef26f632ac69daa28ca61d2fd15eef57a59152
Day Nwosu <kaizenbergltd@gmail.com>
2023-11-12 12:07:56 +0100

    int handler

--- int_handler.c
+++ int_handler.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * int_handler - function that handles inters
+ * @params: va_list parameter passed to int_handler
+ * Return: int
+ */
 int int_handler(va_list params)
 {
     int num, param_digits, last_digit, divisor, counter;
@@ -7,17 +12,17 @@ int int_handler(va_list params)
     param_digits = 0;
     counter = 1;
     divisor = 1;
-    if (num == 0)/*when number is 0*/
+    if (num == 0) /*when number is 0*/
     {
         _putchar(0);
         return;
     }
-    if (num > 0)/*get length of num as param_digits*/
+    if (num > 0) /*get length of num as param_digits*/
     {
         last_digit /= 10;
         param_digits++;
     }
-    while (param_digits > 0)/*print each digit*/
+    while (param_digits > 0) /*print each digit*/
     {
         while (counter < param_digits)
         {
