f2ef9f4ded10ca44b1a603f9c97b4c54b82f441f
Nwosu Friday <fanwosu@gmail.com>
2023-11-12 05:46:50 -0800

    int handler

--- int_handler.c
+++ int_handler.c
@@ -6,29 +6,29 @@
  */
 int int_handler(va_list params)
 {
-	int num, param_digits, last_digit, divisor, counter, returned_digits;
+	int num, param_digits, last_digit, divisor, counter, reserved_digits;
 
 	num = va_arg(params, int);
 	param_digits = 0;
 	counter = 1;
 	divisor = 1;
 	last_digit = 1;
-	if (num == 0) /*when numberis 0*/
+	if (num == 0) /*when number is 0*/
 	{
 		_putchar(0);
-		return(0);
-	}
-	if (num > 0) /*get length of num as param_digits*/
-	{
-		last_digit /= 10;
-		param_digits++;
+		return (0);
 	}
 	if (num < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
-	returned_digits = param_digits;
+	while (num > 0) /*get length of num as param_digits*/
+	{
+		last_digit /= 10;
+		param_digits++;
+	}
+	reserved_digits = param_digits;
 	while (param_digits > 0) /*print each digit*/
 	{
 		while (counter < param_digits)
@@ -41,5 +41,5 @@ int int_handler(va_list params)
 		num %= divisor;
 		param_digits--;
 	}
-	return (returned_digits);
+	return (reserved_digits);
 }
