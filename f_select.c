4ecdbc861e0727f5b59a37ca2836ab1a4c9c8211
Ok-michael <okomichaelakpu@gmail.com>
2023-11-14 12:08:52 +0100

    betty some files

--- f_select.c
+++ f_select.c
@@ -17,7 +17,5 @@ int f_select(char specifier, va_list params)
 		return (s_handler(params));
 	if (specifier == 'd' || specifier == 'i')
 		return (int_handler(params));
-	/*if (specifier == 'b')
-		return (bin_handler(params));*/
 	return (0);
 }
