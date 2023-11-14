c9a59c38dd2dfbeec11cfbcc3bc491953d0842e3
Ok-michael <okomichaelakpu@gmail.com>
2023-11-14 11:38:08 +0100

    fix a semantic on binary_hanler.c

---
+++ binary_handler.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * bin_handler - converts a pass args to it's binary equiv and prints to stdin
+ * @params: this is variable part of the function
+ * Return: the number of characters printed.
+ */
+int bin_handler(va_list params)
+{
+	int binary[32];
+	int i = 0, ctr = 0;
+	unsigned int integer  = va_arg(lst, int);
+
+	if (integer == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+
+	while (integer)
+	{
+		binary[i++] = params % 2;
+		params /= 2;
+		ctr++;
+	}
+
+	while (--i >= 0)
+	{
+		_putchar(bin[i] + '0');
+	}
+
+	return (ctr);
+}
